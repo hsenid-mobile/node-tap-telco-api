95e9253e2494fc8be98ab6c5e2f201db6d2c1ef1
isuruanu <isuru.develop@gmail.com>
2015-04-06 03:19:00 +0530

    Transport - json parsing added.

--- test/transport.js
+++ test/transport.js
@@ -9,6 +9,7 @@ describe("Request creation should be successful", function(){
         sms.requestCreator({applicationId : "APP_000101", password : "password"}).broadcast("test", function(mtReq){
             transport.createRequest({hostname: '127.0.0.1', port: 7000, path: '/sms/send'}, mtReq, function(request){
                 transport.httpClient(request, function(result){
+                    result.statusCode
                     console.log(result)
                 })
             });
