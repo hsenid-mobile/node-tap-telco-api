6b49cb87df90e7b696f0995452e839547355554e
isuruanu <isuru.develop@gmail.com>
2015-04-05 21:57:16 +0530

    tap-api transport - Header added for content type

--- test/transport.js
+++ test/transport.js
@@ -3,8 +3,21 @@ var assert = require('chai').assert;
 
 describe("Request creation should be successful", function(){
     it("Request creation should be successful", function(){
-        transport.createRequest({hostname: 'www.google.com', port: 80, path: '/upload'}, {msg: "hello"}, function(request){
-            assert.equal(request.options.headers['Content-Type'], "application/json");
+
+        var mtReq = {
+            message:"Hello",
+            password:"password",
+            sourceAddress:"77000",
+            destinationAddresses:["tel:94232323232"],
+            applicationId:"APP_999999",
+            encoding:"0",
+            version:"1.0"
+        };
+
+        transport.createRequest({hostname: '127.0.0.1', port: 7000, path: '/sms/send'}, mtReq, function(request){
+            transport.httpClient(request, function(result){
+                console.log(result)
+            })
         });
     })
 });
